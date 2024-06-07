b272402a942b5b6165aee38160ce9ea65ff3f276
Harminder Virk <virk.officials@gmail.com>
2024-06-07 11:47:32 +0530

    feat: add disk space health check

---
+++ src/checks/disk_space.ts
@@ -0,0 +1,86 @@
+/*
+ * @adonisjs/health
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import checkDiskSpace from 'check-disk-space'
+
+import { Result } from '../result.js'
+import { BaseCheck } from '../base_check.js'
+import type { HealthCheckResult } from '../types.js'
+
+/**
+ * Checks for the disk space and report warning and errors after a
+ * certain threshold is exceeded.
+ */
+export class DiskSpaceCheck extends BaseCheck {
+  #warnThreshold: number = 60
+  #failThreshold: number = 80
+  #computeFn: () => Promise<{ free: number; size: number }> = () => {
+    // @ts-expect-error "Broken typings"
+    return checkDiskSpace(this.diskPath)
+  }
+
+  name: string = 'disk_space_check'
+  diskPath = process.platform === 'win32' ? 'C://' : '/'
+
+  /**
+   * Define the percentage threshold after which a
+   * warning should be created
+   */
+  warnWhenExceeds(valueInPercentage: number) {
+    this.#warnThreshold = valueInPercentage
+    return this
+  }
+
+  /**
+   * Define the percentage threshold after which an
+   * error should be created
+   */
+  failWhenExceeds(valueInPercentage: number) {
+    this.#failThreshold = valueInPercentage
+    return this
+  }
+
+  /**
+   * Define a custom callback to compute the disk space. Defaults to
+   * using "check-disk-space" package
+   */
+  compute(callback: () => Promise<{ free: number; size: number }>): this {
+    this.#computeFn = callback
+    return this
+  }
+
+  async run(): Promise<HealthCheckResult> {
+    const { free, size } = await this.#computeFn()
+    const usedPercentage = ((size - free) / size) * 100
+
+    if (usedPercentage >= this.#failThreshold) {
+      return Result.failed('Disk usage exceeded the set threshold').mergeMetaData({
+        percentages: {
+          used: usedPercentage,
+          threshold: this.#failThreshold,
+        },
+      })
+    }
+
+    if (usedPercentage >= this.#warnThreshold) {
+      return Result.warning('Disk usage exceeded the set threshold').mergeMetaData({
+        percentages: {
+          used: usedPercentage,
+          threshold: this.#warnThreshold,
+        },
+      })
+    }
+
+    return Result.ok('Disk usage is under the defined thresholds').mergeMetaData({
+      percentages: {
+        used: usedPercentage,
+      },
+    })
+  }
+}
---
+++ tests/checks/disk_space.spec.ts
@@ -0,0 +1,72 @@
+/*
+ * @adonisjs/health
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { test } from '@japa/runner'
+import { DiskSpaceCheck } from '../../src/checks/disk_space.js'
+
+test.group('Disk space', () => {
+  test('report error when disk space exceeds the defined error threshold', async ({ expect }) => {
+    const diskSpaceCheck = new DiskSpaceCheck().failWhenExceeds(80)
+    diskSpaceCheck.compute(async () => {
+      return {
+        free: 1,
+        size: 10,
+      }
+    })
+
+    expect(await diskSpaceCheck.run()).toEqual({
+      status: 'error',
+      finishedAt: expect.any(Date),
+      message: 'Disk usage exceeded the set threshold',
+      meta: {
+        percentages: {
+          threshold: 80,
+          used: 90,
+        },
+      },
+    })
+  })
+
+  test('report error when disk space exceeds the defined warning threshold', async ({ expect }) => {
+    const diskSpaceCheck = new DiskSpaceCheck().warnWhenExceeds(60)
+    diskSpaceCheck.compute(async () => {
+      return {
+        free: 3,
+        size: 10,
+      }
+    })
+
+    expect(await diskSpaceCheck.run()).toEqual({
+      status: 'warning',
+      finishedAt: expect.any(Date),
+      message: 'Disk usage exceeded the set threshold',
+      meta: {
+        percentages: {
+          threshold: 60,
+          used: 70,
+        },
+      },
+    })
+  })
+
+  test('compute actual disk space and prepare result', async ({ expect }) => {
+    const diskSpaceCheck = new DiskSpaceCheck().warnWhenExceeds(90).failWhenExceeds(99)
+
+    expect(await diskSpaceCheck.run()).toEqual({
+      status: 'ok',
+      finishedAt: expect.any(Date),
+      message: 'Disk usage is under the defined thresholds',
+      meta: {
+        percentages: {
+          used: expect.any(Number),
+        },
+      },
+    })
+  })
+})
