044f3ab959e333c6171efabc59b417fe879d7ef5
Théo LUDWIG <contact@theoludwig.fr>
2025-11-10 12:01:35 +0100

    feat: use % thresholds for `MemoryHeapCheck` and `MemoryRSSCheck`

--- tests/checks/heap.spec.ts
+++ tests/checks/heap.spec.ts
@@ -8,9 +8,36 @@
  */
 
 import { test } from '@japa/runner'
+import v8 from 'node:v8'
+import { mock } from 'node:test'
 import { MemoryHeapCheck } from '../../src/checks/heap_check.ts'
+import stringHelpers from '@poppinss/utils/string'
 
-test.group('Memory Heap', () => {
+const maximumHeapSize = stringHelpers.bytes.parse('250 mb') as number
+
+test.group('Memory Heap - byte thresholds', (group) => {
+  group.each.setup(() => {
+    mock.method(v8, 'getHeapStatistics', () => {
+      return {
+        heap_size_limit: maximumHeapSize,
+        total_heap_size: 0,
+        total_heap_size_executable: 0,
+        total_physical_size: 0,
+        total_available_size: 0,
+        used_heap_size: 0,
+        malloced_memory: 0,
+        peak_malloced_memory: 0,
+        does_zap_garbage: 0,
+        number_of_native_context: 0,
+        number_of_detached_context: 0,
+      }
+    })
+
+    return () => {
+      mock.reset()
+    }
+  })
+
   test('report error when heap usage exceeds the define error threshold', async ({ expect }) => {
     const heapHealthCheck = new MemoryHeapCheck().failWhenExceeds('1mb')
 
@@ -60,6 +87,160 @@ test.group('Memory Heap', () => {
           warningThreshold: 262144000,
           used: expect.any(Number),
         },
+      },
+    })
+  })
+})
+
+test.group('Memory Heap - percentage threshold', (group) => {
+  group.each.setup(() => {
+    mock.method(v8, 'getHeapStatistics', () => {
+      return {
+        heap_size_limit: maximumHeapSize,
+        total_heap_size: 0,
+        total_heap_size_executable: 0,
+        total_physical_size: 0,
+        total_available_size: 0,
+        used_heap_size: 0,
+        malloced_memory: 0,
+        peak_malloced_memory: 0,
+        does_zap_garbage: 0,
+        number_of_native_context: 0,
+        number_of_detached_context: 0,
+      }
+    })
+
+    return () => {
+      mock.reset()
+    }
+  })
+
+  test('report error when heap usage exceeds the defined percentage error threshold', async ({
+    expect,
+  }) => {
+    const errorPercentageThreshold = 90
+    const warningPercentageThreshold = 75
+    const heapUsedBytes = Math.floor((errorPercentageThreshold / 100) * maximumHeapSize) + 1
+
+    mock.method(process, 'memoryUsage', () => {
+      return {
+        rss: 0,
+        heapTotal: 0,
+        heapUsed: heapUsedBytes,
+        external: 0,
+        arrayBuffers: 0,
+      }
+    })
+
+    const heapHealthCheck = new MemoryHeapCheck()
+      .warnWhenExceedsPercentage(warningPercentageThreshold)
+      .failWhenExceedsPercentage(errorPercentageThreshold)
+
+    const result = await heapHealthCheck.run()
+    const usedPercentage = Math.floor((heapUsedBytes / maximumHeapSize) * 100)
+
+    expect(result.status).toEqual('error')
+    expect(result.message).toEqual(
+      `Heap usage is ${usedPercentage}%, which is above the threshold of ${errorPercentageThreshold}%`
+    )
+    expect(result.finishedAt).toEqual(expect.any(Date))
+    expect(result.meta).toEqual({
+      sizeInPercentage: {
+        used: usedPercentage,
+        failureThreshold: errorPercentageThreshold,
+        warningThreshold: warningPercentageThreshold,
+      },
+      heapInBytes: {
+        used: heapUsedBytes,
+        maxHeapSize: maximumHeapSize,
+        failureThreshold: Math.floor((errorPercentageThreshold / 100) * maximumHeapSize),
+        warningThreshold: Math.floor((warningPercentageThreshold / 100) * maximumHeapSize),
+      },
+    })
+  })
+
+  test('report warning when heap usage exceeds the defined percentage warning threshold', async ({
+    expect,
+  }) => {
+    const warningPercentageThreshold = 80
+    const errorPercentageThreshold = 90
+    const heapUsedBytes = Math.floor((warningPercentageThreshold / 100) * maximumHeapSize) + 1
+
+    mock.method(process, 'memoryUsage', () => {
+      return {
+        rss: 0,
+        heapTotal: 0,
+        heapUsed: heapUsedBytes,
+        external: 0,
+        arrayBuffers: 0,
+      }
+    })
+
+    const heapHealthCheck = new MemoryHeapCheck()
+      .warnWhenExceedsPercentage(warningPercentageThreshold)
+      .failWhenExceedsPercentage(errorPercentageThreshold)
+
+    const result = await heapHealthCheck.run()
+    const usedPercentage = Math.floor((heapUsedBytes / maximumHeapSize) * 100)
+
+    expect(result.status).toEqual('warning')
+    expect(result.message).toEqual(
+      `Heap usage is ${usedPercentage}%, which is above the threshold of ${warningPercentageThreshold}%`
+    )
+    expect(result.finishedAt).toEqual(expect.any(Date))
+    expect(result.meta).toEqual({
+      sizeInPercentage: {
+        used: usedPercentage,
+        failureThreshold: errorPercentageThreshold,
+        warningThreshold: warningPercentageThreshold,
+      },
+      heapInBytes: {
+        used: heapUsedBytes,
+        maxHeapSize: maximumHeapSize,
+        failureThreshold: Math.floor((errorPercentageThreshold / 100) * maximumHeapSize),
+        warningThreshold: Math.floor((warningPercentageThreshold / 100) * maximumHeapSize),
+      },
+    })
+  })
+
+  test('prepare ok result when heap usage is under defined percentage thresholds', async ({
+    expect,
+  }) => {
+    const warningPercentageThreshold = 75
+    const errorPercentageThreshold = 80
+    const heapUsedBytes = Math.floor((warningPercentageThreshold / 100) * maximumHeapSize) - 1
+
+    mock.method(process, 'memoryUsage', () => {
+      return {
+        rss: 0,
+        heapTotal: 0,
+        heapUsed: heapUsedBytes,
+        external: 0,
+        arrayBuffers: 0,
+      }
+    })
+
+    const heapHealthCheck = new MemoryHeapCheck()
+      .warnWhenExceedsPercentage(warningPercentageThreshold)
+      .failWhenExceedsPercentage(errorPercentageThreshold)
+
+    const result = await heapHealthCheck.run()
+    const usedPercentage = Math.floor((heapUsedBytes / maximumHeapSize) * 100)
+
+    expect(result.status).toEqual('ok')
+    expect(result.message).toEqual('Heap usage is under defined thresholds')
+    expect(result.finishedAt).toEqual(expect.any(Date))
+    expect(result.meta).toEqual({
+      sizeInPercentage: {
+        used: usedPercentage,
+        failureThreshold: errorPercentageThreshold,
+        warningThreshold: warningPercentageThreshold,
+      },
+      heapInBytes: {
+        used: heapUsedBytes,
+        maxHeapSize: maximumHeapSize,
+        failureThreshold: Math.floor((errorPercentageThreshold / 100) * maximumHeapSize),
+        warningThreshold: Math.floor((warningPercentageThreshold / 100) * maximumHeapSize),
       },
     })
   })
--- tests/checks/rss.spec.ts
+++ tests/checks/rss.spec.ts
@@ -8,9 +8,24 @@
  */
 
 import { test } from '@japa/runner'
+import os from 'node:os'
+import { mock } from 'node:test'
 import { MemoryRSSCheck } from '../../src/checks/rss_check.ts'
+import stringHelpers from '@poppinss/utils/string'
 
-test.group('Memory RSS', () => {
+const totalSystemMemory = stringHelpers.bytes.parse('4GB') as number
+
+test.group('Memory RSS - byte threshold', (group) => {
+  group.each.setup(() => {
+    mock.method(os, 'totalmem', () => {
+      return totalSystemMemory
+    })
+
+    return () => {
+      mock.reset()
+    }
+  })
+
   test('report error when RSS exceeds the define error threshold', async ({ expect }) => {
     const rssHealthCheck = new MemoryRSSCheck().failWhenExceeds('1mb')
 
@@ -58,6 +73,142 @@ test.group('Memory RSS', () => {
           warningThreshold: 335544320,
           used: expect.any(Number),
         },
+      },
+    })
+  })
+})
+
+test.group('Memory RSS - percentage threshold', (group) => {
+  group.each.setup(() => {
+    mock.method(os, 'totalmem', () => {
+      return totalSystemMemory
+    })
+
+    return () => {
+      mock.reset()
+    }
+  })
+
+  test('report error when RSS exceeds the defined error threshold', async ({ expect }) => {
+    const errorPercentageThreshold = 90
+    const warningPercentageThreshold = 75
+    const rssUsedBytes = Math.floor((errorPercentageThreshold / 100) * totalSystemMemory) + 1
+
+    mock.method(process, 'memoryUsage', () => {
+      return {
+        rss: rssUsedBytes,
+        heapTotal: 0,
+        heapUsed: 0,
+        external: 0,
+        arrayBuffers: 0,
+      }
+    })
+
+    const rssHealthCheck = new MemoryRSSCheck()
+      .failWhenExceedsPercentage(errorPercentageThreshold)
+      .warnWhenExceedsPercentage(warningPercentageThreshold)
+
+    const result = await rssHealthCheck.run()
+    const usedPercentage = Math.floor((rssUsedBytes / totalSystemMemory) * 100)
+
+    expect(result.status).toEqual('error')
+    expect(result.message).toEqual(
+      `RSS usage is ${usedPercentage}%, which is above the threshold of ${errorPercentageThreshold}%`
+    )
+    expect(result.finishedAt).toEqual(expect.any(Date))
+    expect(result.meta).toEqual({
+      sizeInPercentage: {
+        used: usedPercentage,
+        failureThreshold: errorPercentageThreshold,
+        warningThreshold: warningPercentageThreshold,
+      },
+      memoryInBytes: {
+        used: rssUsedBytes,
+        totalSystemMemory: totalSystemMemory,
+        failureThreshold: Math.floor((errorPercentageThreshold / 100) * totalSystemMemory),
+        warningThreshold: Math.floor((warningPercentageThreshold / 100) * totalSystemMemory),
+      },
+    })
+  })
+
+  test('report warning when RSS exceeds the defined warning threshold', async ({ expect }) => {
+    const warningPercentageThreshold = 80
+    const errorPercentageThreshold = 90
+    const rssUsedBytes = Math.floor((warningPercentageThreshold / 100) * totalSystemMemory) + 1
+
+    mock.method(process, 'memoryUsage', () => {
+      return {
+        rss: rssUsedBytes,
+        heapTotal: 0,
+        heapUsed: 0,
+        external: 0,
+        arrayBuffers: 0,
+      }
+    })
+
+    const rssHealthCheck = new MemoryRSSCheck()
+      .warnWhenExceedsPercentage(warningPercentageThreshold)
+      .failWhenExceedsPercentage(errorPercentageThreshold)
+
+    const result = await rssHealthCheck.run()
+    const usedPercentage = Math.floor((rssUsedBytes / totalSystemMemory) * 100)
+
+    expect(result.status).toEqual('warning')
+    expect(result.message).toEqual(
+      `RSS usage is ${usedPercentage}%, which is above the threshold of ${warningPercentageThreshold}%`
+    )
+    expect(result.finishedAt).toEqual(expect.any(Date))
+    expect(result.meta).toEqual({
+      sizeInPercentage: {
+        used: usedPercentage,
+        failureThreshold: errorPercentageThreshold,
+        warningThreshold: warningPercentageThreshold,
+      },
+      memoryInBytes: {
+        used: rssUsedBytes,
+        totalSystemMemory: totalSystemMemory,
+        failureThreshold: Math.floor((errorPercentageThreshold / 100) * totalSystemMemory),
+        warningThreshold: Math.floor((warningPercentageThreshold / 100) * totalSystemMemory),
+      },
+    })
+  })
+
+  test('prepare ok result when RSS usage is under defined thresholds', async ({ expect }) => {
+    const warningPercentageThreshold = 75
+    const errorPercentageThreshold = 80
+    const rssUsedBytes = Math.floor((warningPercentageThreshold / 100) * totalSystemMemory) - 1
+
+    mock.method(process, 'memoryUsage', () => {
+      return {
+        rss: rssUsedBytes,
+        heapTotal: 0,
+        heapUsed: 0,
+        external: 0,
+        arrayBuffers: 0,
+      }
+    })
+
+    const rssHealthCheck = new MemoryRSSCheck()
+      .warnWhenExceedsPercentage(warningPercentageThreshold)
+      .failWhenExceedsPercentage(errorPercentageThreshold)
+
+    const result = await rssHealthCheck.run()
+    const usedPercentage = Math.floor((rssUsedBytes / totalSystemMemory) * 100)
+
+    expect(result.status).toEqual('ok')
+    expect(result.message).toEqual('RSS usage is under defined thresholds')
+    expect(result.finishedAt).toEqual(expect.any(Date))
+    expect(result.meta).toEqual({
+      sizeInPercentage: {
+        used: usedPercentage,
+        failureThreshold: errorPercentageThreshold,
+        warningThreshold: warningPercentageThreshold,
+      },
+      memoryInBytes: {
+        used: rssUsedBytes,
+        totalSystemMemory: totalSystemMemory,
+        failureThreshold: Math.floor((errorPercentageThreshold / 100) * totalSystemMemory),
+        warningThreshold: Math.floor((warningPercentageThreshold / 100) * totalSystemMemory),
       },
     })
   })
